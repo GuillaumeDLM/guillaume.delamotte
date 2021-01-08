999eb609438a5b2465c64cb51bcf8c3c7d97d53f
Guillaume Delamotte <guillaume.delamotte@hotmail.fr>
2021-01-08 09:55:56 +0100

    Init MVC Structure
    Controllers à faire

---
+++ public_html/tp3/php_mvc_eleves/Views/Home/index.php
@@ -0,0 +1,17 @@
+<?php
+
+include("../../Models/Services/services_view.php");
+
+html_header();
+
+?>
+<p><a href='../Solutions/solution1.php'>Solution 1 (jointure)</a></p>
+<p><a href='../Solutions/solution2.php'>Solution 2 (plusieurs requêtes)</a></p>
+
+
+<?php
+
+html_footer();
+
+
+?>
---
+++ public_html/tp3/php_mvc_eleves/conf/config.php
@@ -0,0 +1,10 @@
+<?php
+$HOST="127.0.0.1";
+$PORT=8889;
+$DTBS="tpcurseurs";
+$USER="tpcurseurs";
+$PASS="tpcurseurs"; 
+
+$DETAILS=1;
+
+?>
